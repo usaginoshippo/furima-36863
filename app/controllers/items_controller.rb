1feaff04a4f1bcbb245a2499f6a1a999b33cada0
usaginoshippo <86910445+usaginoshippo@users.noreply.github.com>
2021-12-26 20:38:46 +0900

    編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.buy.present? || (@item.user_id == !current_user.id)
+    if @item.user_id != current_user.id || @item.buy.present?
       redirect_to root_path
     end
   end
